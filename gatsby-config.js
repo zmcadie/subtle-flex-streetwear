e9f4e8f9ca2c8c3ad7e124533021bc3470112bbc
Zola McAdie <zolamcadie@Zolas-MacBook-Air.local>
2020-12-10 01:44:44 -0500

    Change env variables prod config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+  })
+}
 
 module.exports = {
   siteMetadata: {
