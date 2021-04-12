c7e33bc24b15b61a8f29754040574930dcbbace9
Zola McAdie <zolamcadie@Zolas-MacBook-Air.local>
2021-04-12 13:36:34 -0400

    Update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Subtle Flex Streetwear',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'We are sellers of streetwear, vintage, thrifted clothing, accessories and shoes.',
   },
   flags: {
     PRESERVE_WEBPACK_CACHE: true,
